41755b589152bd5d47e8b9253dd72c47e03b668a
gedunlap <g.e.dunlap90@gmail.com>
2026-06-05 12:22:48 -0400

    landing page done except mobile navbar

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -6,12 +6,20 @@ const Landing = () => {
     return (
       <div className='landing'>
         <div>
-            <button className='landingButton'>Book St Pete</button>
-            <img className='homelogosp' src={mspLogo} alt="logo" />
+            <a href='https://www.fresha.com/a/material-st-pete-st-petersburg-1957-1st-avenue-south-jemgscy9?pId=537575' target='_blank' rel='noreferrer'>
+                <button className='landingButton'>
+                    Book St Pete
+                </button>
+            </a>
+            <a href='/sp/home'><img className='homelogosp' src={mspLogo} alt="logo" /></a>
         </div>
         <div>
-            <button className='landingButton'>Book Tampa</button>
-            <img className='homelogotp' src={tpaLogo} alt="logo" />
+            <a href='https://www.fresha.com/a/material-tampa-tampa-2502-north-howard-avenue-d9hvukdk?preview=e701202d-d15a-4769-9fa3-3bfaa7f11c85&share&pId=537575' target='_blank' rel='noreferrer'>
+                <button className='landingButton'>
+                    Book Tampa
+                </button>
+            </a>
+            <a href='/tp/home'><img className='homelogotp' src={tpaLogo} alt="logo" /></a>
         </div>        
       </div>
     )
